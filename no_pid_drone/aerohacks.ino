2797d8fd38338b4871bfe3c11908c0a3e00df1de
Maximilian Newman Loussouarn <maximilian.newman@icloud.com>
2026-03-13 19:00:54 -0400

    Update and rename no_pid_drone.ino to aerohacks.ino

--- no_pid_drone/aerohacks.ino
+++ no_pid_drone/aerohacks.ino
@@ -1,16 +1,23 @@
-// Completely untested for now. I just wrote this while I was bored in the 12 hour train ride
-
-
 #include <ESP32Servo.h>
-#include "Wire.h"
-#include <MPU6050_light.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
-MPU6050 mpu(Wire);
+
+#include <WiFi.h>
+IPAddress apIP(192, 168, 4, 1);
+IPAddress netMsk(255, 255, 255, 0);
+WiFiServer tcpServer(8080);
+WiFiClient client;
+
+Adafruit_MPU6050 mpu;
  
 Servo A;
 Servo B;
 Servo C;
 Servo D;
+
+byte mode = 0;
 
 const float MAX_ANGULAR_VELOCITY = 5;
 const float MAX_ANGLE = 20;
@@ -24,13 +31,17 @@ float targetGyroVY = 0;
 float targetGyroX = 0;
 float targetGyroY = 0;
 
+float gyroOffsetX = 0;
+float gyroOffsetY = 0;
+
 float vz = 0;
 float vx = 0;
 float vy = 0;
-
 float x = 0;
 float y = 0;
 float z = 0;
+float gyroX = 0;
+float gyroY = 0;
 
 int thrustA = 0;
 int thrustB = 0;
@@ -40,20 +51,57 @@ int thrustD = 0;
 unsigned long lastTime = 0;
  
 void setup() {
-	//ESP32PWM::allocateTimer(0);
-	//ESP32PWM::allocateTimer(1);
-	//ESP32PWM::allocateTimer(2);
-	//ESP32PWM::allocateTimer(3);
+  Serial.begin(115200);
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+
+
 	A.setPeriodHertz(50);
-	A.attach(2, 500, 2400);
+	A.attach(5, 2, 19000);
 	B.setPeriodHertz(50);
-	B.attach(4, 500, 2400);
+	B.attach(6, 2, 19000);
 	C.setPeriodHertz(50);
-	C.attach(12, 500, 2400);
+	C.attach(3, 2, 19000);
 	D.setPeriodHertz(50);
-	D.attach(13, 500, 2400);
+	D.attach(4, 2, 19000);
+
+  Wire.begin(11,10);
+  if (!mpu.begin(0x68)) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {delay(10);}
+  }
+  //mpu.calcOffsets(true, true);
+  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+  
+  sensors_event_t a, g, temp;
+
+  unsigned int numCalibReadings = 2000;
+
+  for (unsigned int i=0; i<numCalibReadings; i++) {
+    mpu.getEvent(&a, &g, &temp);
+    gyroOffsetX += g.gyro.x;
+    gyroOffsetY += g.gyro.y;
+    delay(2);
+  }
+  gyroOffsetX /= numCalibReadings;
+  gyroOffsetY /= numCalibReadings;
+
+  WiFi.softAPConfig(apIP, apIP, netMsk);
+  WiFi.softAP("AeroHacks Drone 1", "skibidi123");
+  tcpServer.begin();
+
   lastTime = millis();
 }
+
+
+
+
+
+
+
 
 void loop() {
   unsigned long newTime = millis();
@@ -61,17 +109,59 @@ void loop() {
   lastTime = newTime;
 
 
-  mpu.update();
-  float gyroX = mpu.getAngleX();
-  float gyroY = mpu.getAngleY();
-  float gyroVX = mpu.getGyroX();
-  float gyroVY = mpu.getGyroY();
-  float rawAccZ = mpu.getAccZ();
-  float rawAccX = mpu.getAccX();
-  float rawAccY = mpu.getAccY();
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  float gyroVX = g.gyro.x - gyroOffsetX;
+  float gyroVY = g.gyro.y - gyroOffsetY;
+  gyroX -= gyroVX * dt;
+  gyroY -= gyroVY * dt;
+  float rawAccZ = a.acceleration.z;
+  float rawAccX = a.acceleration.x;
+  float rawAccY = a.acceleration.y;
   float accZ = rawAccZ * cos(gyroX * PI/180) * cos(gyroY * PI/180) - rawAccX * sin(gyroX * PI/180) - rawAccY * sin(gyroY * PI/180);
   float accX = rawAccX * cos(gyroX * PI/180) + rawAccZ * sin(gyroX * PI/180);
   float accY = rawAccY * cos(gyroY * PI/180) + rawAccZ * sin(gyroY * PI/180);
+
+
+
+  if (!client) {client = tcpServer.available();}
+  else if (!client.connected()) {
+    client.stop();
+    mode = 0;
+  }
+
+  if (client.available()) {
+    String instruct = client.readStringUntil('\n');
+
+    if (instruct == "ping") {
+      client.print("ping");
+    }
+
+    else if (instruct == "angX") {client.print(String(gyroX));}
+    else if (instruct == "angY") {client.print(String(gyroY));}
+    else if (instruct == "gyroX") {client.print(String(gyroVX));}
+    else if (instruct == "gyroY") {client.print(String(gyroVY));}
+    else if (instruct == "gMode") {client.print(String(mode));}
+    
+    else if (instruct.startsWith("mode")) {
+      instruct.remove(0, 4);
+      mode = instruct.toInt();
+      Serial.print("New Mode: ");
+      Serial.println(mode);
+    }
+
+    else if (instruct == "manT") {
+      thrustA = client.readStringUntil(',').toInt();
+      thrustB = client.readStringUntil(',').toInt();
+      thrustC = client.readStringUntil(',').toInt();
+      thrustD = client.readStringUntil('\n').toInt();
+    }
+    client.print("\n");
+  }
+
+
+  //Serial.println(gyroX);
 
 
   if (z < targetZ) {targetVZ += 1 * dt;}
@@ -155,6 +245,13 @@ void loop() {
   vx += accX * dt;
   vy += accY * dt;
   vz += accZ * dt;
+
+  if (mode == 0) {
+    thrustA = 0;
+    thrustB = 0;
+    thrustC = 0;
+    thrustD = 0;
+  }
 
   A.write(thrustA);
   B.write(thrustB);
